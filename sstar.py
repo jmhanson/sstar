2c3546e447d2b8586b2eec3b5842b9ae96a6c846
Jeremy Hanson <jmhanson@gmail.com>
2026-01-01 20:02:15 -0600

    Switch to more standard Python script form and switch to using argparse.

--- sstar.py
+++ sstar.py
@@ -8,7 +8,7 @@
 # 20210927 JMH Created, tracking changes in git.
 
 import math
-import sys
+import argparse
 
 # ------------------------------------------------------------
 # Inits
@@ -18,6 +18,7 @@ pageSize = (612, 792)
 marginSize = 18
 
 outFile = 'sstar.ps'
+
 
 # ------------------------------------------------------------
 # Subroutines
@@ -30,6 +31,7 @@ def write_tri(fb, triX, triY):
     fb.write('%f %f lineto\n' % (triX[2], triY[2]))            
     fb.write('closepath\n')
     fb.write('fill\n')
+
 
 def xform_tri(mat, triX, triY):
     """ Apply 2x2 linear transformation defined in mat. """
@@ -46,77 +48,91 @@ def xform_tri(mat, triX, triY):
     return tx, ty
 
 
-# ------------------------------------------------------------
-# Process inputs
-
-argc = len(sys.argv)
-if argc > 1:
-    order = int(sys.argv[1])
-else:
-    order = 20
-assert order > 1, 'order must be larger than 1'
+def main(argv=None):
 
 
-# ------------------------------------------------------------
-# Preliminary Calculations
+    # ------------------------------------------------------------
+    # Process inputs
 
-# Coordinate system
-transX = pageSize[0]/2
-transY = pageSize[1]/2
-xMax = transX - marginSize
-yMax = transY - marginSize
-dMin = min(xMax, yMax)
-#scale = 1.0/dMin
-scale = dMin
-
-# Angle spanned by a triangle (order triangles, spaced theta apart)
-theta = math.pi/order
-
-# Triangle (spoke) primitive, with the primary vertex at (0,0),
-# radial edges of length 1.0, and centerline along X. Note
-# that we don't close the path here. Apparently PS can do
-# this on its own.
-thetaH = theta/2.0
-triH = math.sin(thetaH)
-triL = math.cos(thetaH)
-triX = [0.0, triL,      triL]
-triY = [0.0, triH, -1.0*triH]
-
-# Incremental rotation matrix, row-major order
-thetaStep = theta*2.0
-cts = math.cos(thetaStep)
-sts = math.sin(thetaStep)
-rot = [cts, -1.0*sts, sts, cts]
+    me = "sstar.py"
+    parser = argparse.ArgumentParser(prog=me,
+        description="Draw a Siemens Star target with n spokes."
+        )
+    parser.add_argument("n",
+        nargs='?',
+        type=int,
+        help="Desired number of star spokes, minimum: 2, default: 20",
+        default=20,
+        )
+    args = parser.parse_args(argv)
+    assert args.n > 1, "Input n must be larger than 1"
 
 
-# ------------------------------------------------------------
-# Write ps file
+    # ------------------------------------------------------------
+    # Preliminary Calculations
 
-with open(outFile, 'w') as fb:
+    # Coordinate system
+    transX = pageSize[0]/2
+    transY = pageSize[1]/2
+    xMax = transX - marginSize
+    yMax = transY - marginSize
+    dMin = min(xMax, yMax)
+    scale = dMin
 
-    # Header
-    fb.write('%!PS-Adobe-2.0\n')
-    fb.write('%%Creator: ' + sys.argv[0] + '\n')
-    fb.write('%%BoundingBox: 0 0 width height\n')
-    fb.write('%%LanguageLevel: 2\n')
-    fb.write('%%Pages: 1\n')
+    # Angle spanned by a triangle (n triangles, spaced theta apart)
+    theta = math.pi/args.n
 
-    # Page size
-    fb.write('<< /PageSize [%s %s] >> setpagedevice\n' % pageSize)
+    # Triangle (spoke) primitive, with the primary vertex at (0,0),
+    # radial edges of length 1.0, and centerline along X. Note
+    # that we don't close the path here. Apparently PS can do
+    # this on its own.
+    thetaH = theta/2.0
+    triH = math.sin(thetaH)
+    triL = math.cos(thetaH)
+    triX = [0.0, triL,      triL]
+    triY = [0.0, triH, -1.0*triH]
 
-    # Coordinate transformation
-    fb.write('%s %s translate\n' % (transX, transY))
-    fb.write('%f dup scale\n' % scale)
+    # Incremental rotation matrix, row-major order
+    thetaStep = theta*2.0
+    cts = math.cos(thetaStep)
+    sts = math.sin(thetaStep)
+    rot = [cts, -1.0*sts, sts, cts]
 
-    # First triangle
-    write_tri(fb, triX, triY)
 
-    # Rotate and write remaining triangles
-    for i in range(order-1):
-        triX, triY = xform_tri(rot, triX, triY)
+    # ------------------------------------------------------------
+    # Write ps file
+
+    with open(outFile, 'w') as fb:
+
+        # Header
+        fb.write('%!PS-Adobe-2.0\n')
+        fb.write('%%Creator: ' + me + '\n')
+        fb.write('%%BoundingBox: 0 0 width height\n')
+        fb.write('%%LanguageLevel: 2\n')
+        fb.write('%%Pages: 1\n')
+
+        # Page size
+        fb.write('<< /PageSize [%s %s] >> setpagedevice\n' % pageSize)
+
+        # Coordinate transformation
+        fb.write('%s %s translate\n' % (transX, transY))
+        fb.write('%f dup scale\n' % scale)
+
+        # First triangle
         write_tri(fb, triX, triY)
 
-    fb.write('showpage\n')
-    fb.write('%%EOF\n')
-             
-print('Wrote %s' % outFile)
+        # Rotate and write remaining triangles
+        for i in range(args.n-1):
+            triX, triY = xform_tri(rot, triX, triY)
+            write_tri(fb, triX, triY)
+
+        fb.write('showpage\n')
+        fb.write('%%EOF\n')
+
+    print('Wrote %s' % outFile)
+
+
+# This line makes the script importable without executing main()
+# via import %%NAME
+if __name__ == "__main__":
+    raise SystemExit(main())
